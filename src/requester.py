02b9c6713f8943f7e4ef8e51fa403eb0b144b527
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-21 11:57:45 +0100

    Repaired Stringbuilder

--- src/requester.py
+++ src/requester.py
@@ -5,18 +5,19 @@ markers = ['better', 'easier', 'faster', 'nicer', 'wiser', 'cooler', 'decent', '
 def req(objA, objB, aspect):
     url = buildString(objA, objB, aspect)
     r = requests.get(url)
+    print(r.text)
     hits = r.json()['hits']['hits']
     sentences = []
     for i in range(0, len(hits)):
         sentences.append(hits[i]['_source']['text'])
-        print (hits[i]['_source']['text'])
     return sentences
 
 def buildString(objA, objB, aspect):
-    url = 'http://localhost:9222/commoncrawl2/_search?q=text:{} AND {} AND {} AND'.format(objA, objB, aspect)
+    url = 'http://localhost:9222/commoncrawl2/_search?q=text:{}%20AND%20{}%20AND%20('.format(objA, objB)
     for i in range(0, len(markers) - 1):
         url += markers[i]
         url += '%20OR%20'
     url += markers[len(markers) - 1]
-    url += ')&from=0&size=10'
+    url += ')&from=0&size=100'
+    print(url)
     return url
