87da3c86c74d8bf2e170d40f2587086cbd92f236
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-21 14:01:13 +0100

    Text analysis restored

--- src/requester.py
+++ src/requester.py
@@ -1,11 +1,16 @@
 import requests
+import json
 
 markers = ['better', 'easier', 'faster', 'nicer', 'wiser', 'cooler', 'decent', 'safer', 'superior', 'solid', 'terrific', 'worse', 'harder', 'slower', 'poorly', 'uglier', 'poorer', 'lousy', 'nastier', 'inferior', 'mediocre']
 
 def req(objA, objB, aspect):
     url = buildString(objA, objB, aspect)
     r = requests.get(url)
-    print (r.text)
+    hits = r.json()['hits']['hits']
+    sentences = []
+    for i in range(0, len(hits)):
+        sentences.append(hits[i]['_source']['text'])
+    return sentences
 
 def buildString(objA, objB, aspect):
     url = 'http://localhost:9222/commoncrawl2/_search?q=text:{}%20AND%20{}%20AND%20('.format(objA, objB)
