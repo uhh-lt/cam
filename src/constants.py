f119fd35649b42a43595bde5a24a527161fd9f50
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-22 16:23:55 +0100

    Added a few constants, mainly for aspect_getter

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,22 @@
+import inflect
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+
 MARKERS = ['better', 'easier', 'faster', 'nicer', 'wiser', 'cooler', 'decent', 'safer', 'superior', 'solid', 'terrific',
            'worse', 'harder', 'slower', 'poorly', 'uglier', 'poorer', 'lousy', 'nastier', 'inferior', 'mediocre']
 BETTER_MARKERS = ['better', 'easier', 'faster', 'nicer', 'wiser',
                   'cooler', 'decent', 'safer', 'superior', 'solid', 'terrific']
 WORSE_MARKERS = ['worse', 'harder', 'slower', 'poorly', 'uglier',
                  'poorer', 'lousy', 'nastier', 'inferior', 'mediocre']
-NEG_REMOVERS = ['didn\'t', 'couldn\'t', 'wasn\'t', 'haven\'t', 'wouldn\'t',
-                'did not', 'could not', 'was not', 'have not', 'would not',
-                'didnt', 'couldnt', 'wasnt', 'havent', 'wouldnt']
+NEG_REMOVERS = ['didn\'t', 'couldn\'t', 'wasn\'t', 'haven\'t', 'wouldn\'t', 'can\'t'
+                'did not', 'could not', 'was not', 'have not', 'would not', 'can not'
+                'didnt', 'couldnt', 'wasnt', 'havent', 'wouldnt' 'cannot']
 HOST = 'http://localhost:9222/'
 ESCC2 = 'commoncrawl2/_search?q=text:'
+STOPWORDS = set(stopwords.words('english'))
+NON_ASPECTS = ['come', 'much', 'good', 'even', 'think', 'would', 'well', 'like', 'also', 'nice', 'great', 'made', 'could']
+NUMBER_STRINGS = []
+p = inflect.engine()
+for i in range(0,1000):
+    NUMBER_STRINGS.append(p.number_to_words(i))
