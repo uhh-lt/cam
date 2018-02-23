d976fdb4509ef11e9a906415fe253550f524b2df
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-23 12:22:25 +0100

    find_aspects is now working

--- src/aspect_searcher.py
+++ src/aspect_searcher.py
@@ -2,15 +2,15 @@ import re
 import constants
 
 
-def extract_main_aspects(stringlist, objA, objB):
+def extract_main_aspects(sentences, objA, objB):
     '''
     Extracts the most common words from a list of strings.
 
-    stringlist: List
+    sentences:  List
                 list of strings
     '''
     worddict = {}
-    for s in stringlist:
+    for s in sentences:
         wordlist = re.compile('[A-Za-z]+').findall(s)
         for w in wordlist:
             if w not in constants.STOPWORDS and w not in constants.MARKERS and w != objA and w != objB and w not in constants.NON_ASPECTS and w not in constants.NUMBER_STRINGS:
@@ -26,5 +26,19 @@ def extract_main_aspects(stringlist, objA, objB):
     return result
 
 
-def find_aspect(sentence, aspects):
-    
+def find_aspects(sentence, aspects):
+    '''
+    Searches for one of the aspects the user entered within a given sentence.
+
+    sentence:   String
+                the sentence which shall be scanned for aspects
+
+    aspects:    List
+                list of Aspect objects
+    '''
+    wordlist = re.compile('[A-Za-z]+').findall(sentence)
+    ret_aspects = []
+    for aspect in aspects:
+        if aspect.name in wordlist:
+            ret_aspects.append(aspect)
+    return ret_aspects
