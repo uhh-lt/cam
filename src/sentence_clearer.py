6aca1a5c6da8a9ff528c45e49d9eb3f6796105fb
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-23 14:08:01 +0100

    added remove_wrong_marker_positions

--- src/sentence_clearer.py
+++ src/sentence_clearer.py
@@ -1,7 +1,7 @@
 import constants
 
 
-def clear_sentences(sentences):
+def clear_sentences(sentences, objA, objB):
     '''
     Removes sentences which (currently) cannot be properly analyzed from a list of sentences.
 
@@ -10,6 +10,7 @@ def clear_sentences(sentences):
     '''
     sentences = remove_questions(sentences)
     sentences = remove_negations(sentences)
+    sentences = remove_wrong_marker_positions(sentences, objA, objB)
     return sentences
 
 
@@ -38,4 +39,27 @@ def remove_negations(sentences):
             if neg in s:
                 sentences.remove(s)
                 break
+    return sentences
+
+
+def remove_wrong_marker_positions(sentences, objA, objB):
+    '''
+    Removes sentences in which no marker is between the objects from a list of sentences.
+
+    sentences:  list
+                a list of sentences
+    '''
+    for s in sentences:
+        posA = s.find(objA.name)
+        posB = s.find(objB.name)
+        pos_first = min(posA, posB)
+        pos_second = max(posA, posB)
+        no_marker_found = True
+        for marker in constants.MARKERS:
+            pos_marker = s.find(marker)
+            if pos_marker > pos_first and pos_marker < pos_second:
+                no_marker_found = False
+                break
+        if no_marker_found:
+            sentences.remove(s)
     return sentences
