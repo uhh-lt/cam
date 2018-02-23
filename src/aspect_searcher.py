2d5efc0ac151d16a53f444a7347b2f03191421de
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-23 14:36:27 +0100

    comments

--- src/aspect_searcher.py
+++ src/aspect_searcher.py
@@ -9,18 +9,20 @@ def extract_main_aspects(sentences, objA, objB):
     sentences:  List
                 list of strings
     '''
-    worddict = {}
+    worddict = {}  # stores all words as keys and the number of times they've been found as values
     for s in sentences:
+        # find all words in the sentence
         wordlist = re.compile('[A-Za-z]+').findall(s)
         for w in wordlist:
+            # check if w is "useful" as an aspect
             if w not in constants.STOPWORDS and w not in constants.MARKERS and w != objA and \
-            w != objB and w not in constants.NON_ASPECTS and w not in constants.NUMBER_STRINGS:
+                    w != objB and w not in constants.NON_ASPECTS and w not in constants.NUMBER_STRINGS:
                 if w in worddict:
                     worddict[w] += 1
                 else:
                     worddict[w] = 1
     result = {}
-    for _i in range(0, 10):
+    for _i in range(0, 10):  # return the top 10 aspects
         k = max(worddict, key=worddict.get)
         result[k] = worddict[k]
         worddict.pop(k)
@@ -37,7 +39,8 @@ def find_aspects(sentence, aspects):
     aspects:    List
                 list of Aspect objects
     '''
-    wordlist = re.compile('[A-Za-z]+').findall(sentence)
+    wordlist = re.compile(
+        '[A-Za-z]+').findall(sentence)  # find all words in the sentence
     ret_aspects = []
     for aspect in aspects:
         if aspect.name in wordlist:
