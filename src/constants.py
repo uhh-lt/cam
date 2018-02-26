ca812d817c25976a5a313e3517327cbd91c06ad0
Anna Drogaytseva <6drogayt@informatik.uni-hamburg.de>
2018-02-26 14:10:12 +0100

    added opposite_markers

--- src/constants.py
+++ src/constants.py
@@ -12,6 +12,7 @@ NEGATIVE_MARKERS = ['worse', 'harder', 'slower', 'poorly', 'uglier',
 NEGATIONS = ['didn\'t', 'couldn\'t', 'wasn\'t', 'haven\'t', 'wouldn\'t', 'can\'t'
                 'did not', 'could not', 'was not', 'have not', 'would not', 'can not'
                 'didnt', 'couldnt', 'wasnt', 'havent', 'wouldnt' 'cannot']
+OPPOSITE_MARKERS = ['not', 'no']
 HOSTNAME = 'http://localhost:9222/'
 CRAWL_DATA_REPOS = 'commoncrawl2/_search?q=text:'
 STOPWORDS = set(stopwords.words('english'))
