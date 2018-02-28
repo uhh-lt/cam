8807d138b973392bbd01831b9c0c23a030bdbfe9
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-02-28 16:54:58 +0100

    Importing FormsModule to enable ngModel

--- src/Frontend/camFrontend/src/app/app.module.ts
+++ src/Frontend/camFrontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { HttpClientModule } from "@angular/common/http";
 
@@ -7,7 +8,7 @@ import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
