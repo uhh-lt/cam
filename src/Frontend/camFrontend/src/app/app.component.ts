cec61dc3779011b6f8c1df82bb4b79af13cd5ef7
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-03-01 11:21:30 +0100

    added comments, changed the result save structure

--- src/Frontend/camFrontend/src/app/app.component.ts
+++ src/Frontend/camFrontend/src/app/app.component.ts
@@ -1,51 +1,48 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // needed for the http.get method
 /**
  * UI for the Comparative Argument Machine. Currently everything is done by this one class --
 showing the UI, reading the input and requesting the Elastic Search.
+ *
+ * @export
+ * @class AppComponent
  */
-
-import { Component } from "@angular/core";
-import { HttpClient } from "@angular/common/http"; // needed for the http.get method
-
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "CAM";
+  title = 'CAM';
   aspects = [1]; // the rows of aspects currently shown in the UI
   aspectDict = {}; // the aspects currently entered
   weightDict = { 1: 1 }; // the weightings of the aspects currently chosen with the sliders
   resshow = false; // boolean that checks if the result table should be shown
   loadshow = false; // boolean that checks if the loading screen should be shown
-  object_A = ""; // the first object currently entered
-  object_B = ""; // the second object currently entered
-  A_won = ""; // stores if the first object is the winner or not
-  B_won = ""; // stores if the second object is the winner or not
-  A_score = 0; // stores the score of the first object
-  B_score = 0; // stores the score of the second object
-  A_mainaspects = ""; // stores the main aspects of the first object
-  B_mainaspects = ""; // stores the main aspects of the second object
-  A_sentex = {}; // stores some example sentences for the first object
-  B_sentex = {}; // stores some example sentences for the second object
+  object_A = ''; // the first object currently entered
+  object_B = ''; // the second object currently entered
+  winner_obj = ''; // the first object of the results shown
+  loser_obj = ''; // the second object of the results shown
+  winner_score = 0; // stores the score of the first object
+  loser_score = 0; // stores the score of the second object
+  winner_links = ''; // stores the main links of the first object
+  loser_links = ''; // stores the main links of the second object
+  winner_sentex = {}; // stores some example sentences for the first object
+  loser_sentex = {}; // stores some example sentences for the second object
 
   constructor(private http: HttpClient) {}
 
   /**
    * Reads the input from the UI, starts the search request and calls the save method.
-   *
-   * @memberof AppComponent
    */
   compare() {
     this.loadshow = true; // show the loading screen
     // reset everything to its default and hide the result table
     this.resshow = false;
-    this.A_won = "";
-    this.B_won = "";
-    this.A_mainaspects = "";
-    this.B_mainaspects = "";
-    this.A_sentex = {};
-    this.B_sentex = {};
+    this.winner_links = '';
+    this.loser_links = '';
+    this.winner_sentex = {};
+    this.loser_sentex = {};
     const finalAspDict = {};
     // read the aspects entered by the user and store them with their weight
     for (const aspect of this.aspects) {
@@ -62,88 +59,169 @@ export class AppComponent {
   /**
    * Saves the search result so that they can be shown in the UI.
    *
-   * @param {any} result the search results that should be saved
-   * @memberof AppComponent
+   * @param result the search results that should be saved
    */
   saveResult(result) {
     this.resshow = true; // show the result table
-    // save the scores
-    this.A_score = result["score object 1"];
-    this.B_score = result["score object 2"];
+    const a_won = result['score object 1'] > result['score object 2'];
     // save the winner
-    if (this.A_score > this.B_score) {
-      this.A_won = "X";
-    } else if (this.A_score < this.B_score) {
-      this.B_won = "X";
+    if (a_won) {
+      this.winner_obj = result['object 1'];
+      this.loser_obj = result['object 2'];
     } else {
-      this.A_won = "X";
-      this.B_won = "X";
+      this.winner_obj = result['object 2'];
+      this.loser_obj = result['object 1'];
     }
-    // sort the main aspects for both objects by their values and save them
-    const A_aspkeys = Object.keys(result["main aspects object 1"]);
-    for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-      let maxi = 0;
-      let maxkey = "";
-      for (const key of A_aspkeys) {
-        if (result["main aspects object 1"][key] > maxi) {
-          maxi = result["main aspects object 1"][key];
-          maxkey = key;
-        }
-      }
-      if (A_aspkeys.length > 0) {
-        this.A_mainaspects += `${maxkey}(${maxi})`;
-        const index = A_aspkeys.indexOf(maxkey, 0);
-        if (index > -1) {
-          A_aspkeys.splice(index, 1);
-        }
-        this.A_mainaspects += `, `;
-      } else {
-        break;
-      }
+    // save the scores
+    if (a_won) {
+      this.winner_score = result['score object 1'];
+      this.loser_score = result['score object 2'];
+    } else {
+      this.winner_score = result['score object 2'];
+      this.loser_score = result['score object 1'];
     }
-    const B_aspkeys = Object.keys(result["main aspects object 2"]);
-    for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
-      let maxi = 0;
-      let maxkey = "";
-      for (const key of B_aspkeys) {
-        if (result["main aspects object 2"][key] > maxi) {
-          maxi = result["main aspects object 2"][key];
-          maxkey = key;
+    // sort the main links for both objects by their values and save them
+    if (a_won) {
+      const A_aspkeys = Object.keys(result['main links object 1']);
+      for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        let maxi = 0;
+        let maxkey = '';
+        for (const key of A_aspkeys) {
+          if (result['main links object 1'][key] > maxi) {
+            maxi = result['main links object 1'][key];
+            maxkey = key;
+          }
+        }
+        if (A_aspkeys.length > 0) {
+          this.winner_links += `${maxkey}(${maxi})`;
+          const index = A_aspkeys.indexOf(maxkey, 0);
+          if (index > -1) {
+            A_aspkeys.splice(index, 1);
+          }
+          this.winner_links += `, `;
+        } else {
+          break;
         }
       }
-      if (B_aspkeys.length > 0) {
-        this.B_mainaspects += `${maxkey}(${maxi})`;
-        const index = B_aspkeys.indexOf(maxkey, 0);
-        if (index > -1) {
-          B_aspkeys.splice(index, 1);
+      const B_aspkeys = Object.keys(result['main links object 2']);
+      for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        let maxi = 0;
+        let maxkey = '';
+        for (const key of B_aspkeys) {
+          if (result['main links object 2'][key] > maxi) {
+            maxi = result['main links object 2'][key];
+            maxkey = key;
+          }
         }
-        this.B_mainaspects += `, `;
-      } else {
-        break;
+        if (B_aspkeys.length > 0) {
+          this.loser_links += `${maxkey}(${maxi})`;
+          const index = B_aspkeys.indexOf(maxkey, 0);
+          if (index > -1) {
+            B_aspkeys.splice(index, 1);
+          }
+          this.loser_links += `, `;
+        } else {
+          break;
+        }
+      }
+    } else {
+      const A_aspkeys = Object.keys(result['main links object 1']);
+      for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        let maxi = 0;
+        let maxkey = '';
+        for (const key of A_aspkeys) {
+          if (result['main links object 1'][key] > maxi) {
+            maxi = result['main links object 1'][key];
+            maxkey = key;
+          }
+        }
+        if (A_aspkeys.length > 0) {
+          this.loser_links += `${maxkey}(${maxi})`;
+          const index = A_aspkeys.indexOf(maxkey, 0);
+          if (index > -1) {
+            A_aspkeys.splice(index, 1);
+          }
+          this.loser_links += `, `;
+        } else {
+          break;
+        }
+      }
+      const B_aspkeys = Object.keys(result['main links object 2']);
+      for (const _i of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
+        let maxi = 0;
+        let maxkey = '';
+        for (const key of B_aspkeys) {
+          if (result['main links object 2'][key] > maxi) {
+            maxi = result['main links object 2'][key];
+            maxkey = key;
+          }
+        }
+        if (B_aspkeys.length > 0) {
+          this.winner_links += `${maxkey}(${maxi})`;
+          const index = B_aspkeys.indexOf(maxkey, 0);
+          if (index > -1) {
+            B_aspkeys.splice(index, 1);
+          }
+          this.winner_links += `, `;
+        } else {
+          break;
+        }
       }
     }
     // save the sentences each of the objects has won
     let i = 0;
-    for (const sentence of result["object 1 sentences"]) {
-      this.A_sentex[i++] = sentence;
-    }
-    i = 0;
-    for (const sentence of result["object 2 sentences"]) {
-      this.B_sentex[i++] = sentence;
+    if (a_won) {
+      for (const sentence of result['object 1 sentences']) {
+        this.winner_sentex[i++] = sentence;
+      }
+      i = 0;
+      for (const sentence of result['object 2 sentences']) {
+        this.loser_sentex[i++] = sentence;
+      }
+    } else {
+      for (const sentence of result['object 1 sentences']) {
+        this.loser_sentex[i++] = sentence;
+      }
+      i = 0;
+      for (const sentence of result['object 2 sentences']) {
+        this.winner_sentex[i++] = sentence;
+      }
     }
     this.loadshow = false; // hide the loading screen
   }
 
+  /**
+   * Builds the URL needed for communicating with the server and requesting the search.
+   *
+   * @param objA the first object entered by the user
+   * @param objB the second object entered by the user
+   * @param aspectList the list of aspects with their weights entered by the user
+   * @returns the URL
+   */
   buildURL(objA, objB, aspectList) {
     let URL = this.buildObjURL(objA, objB);
     URL += this.addAspectURL(aspectList);
     return URL;
   }
 
+  /**
+   * Builds the first part of the URL containing the host address and the objects entered.
+   *
+   * @param objA the first object entered by the user
+   * @param objB the second object entered by the user
+   * @returns the first part of the URL
+   */
   buildObjURL(objA, objB) {
     return `http://localhost:5000/cam?objectA=${objA}&objectB=${objB}`;
   }
 
+  /**
+   * Adds a URL part containing the aspects entered by the user to an already existing first part
+   * of a URL.
+   *
+   * @param aspectList the list of aspects with their weights entered by the user
+   * @returns the second part of the URL
+   */
   addAspectURL(aspectList) {
     let url_part = ``;
     let i = 1;
@@ -153,15 +231,30 @@ export class AppComponent {
     return url_part;
   }
 
+  /**
+   * Checks if the user entered something in both the first and the second object fields.
+   *
+   * @returns true, if the user entered something in both fields, false if not
+   */
   objectsEntered() {
-    return this.object_A !== "" && this.object_B !== "";
+    return this.object_A !== '' && this.object_B !== '';
   }
 
+  /**
+   * Adds an aspect to the list of currently shown aspects which makes the UI show a new aspect row
+   *
+   */
   addAspect() {
     this.aspects.push(this.aspects[this.aspects.length - 1] + 1);
     this.weightDict[this.aspects[this.aspects.length - 1]] = 1;
   }
 
+  /**
+   * Removes an aspect from the list of currently shown aspects which makes the UI remove this
+   * aspect row.
+   *
+   * @param aspect the aspect row to be removed, given as a number
+   */
   removeAspect(aspect) {
     if (this.aspects.length > 1) {
       const index = this.aspects.indexOf(aspect, 0);
@@ -169,7 +262,7 @@ export class AppComponent {
         this.aspects.splice(index, 1);
       }
     } else {
-      this.aspectDict[this.aspects[0]] = "";
+      this.aspectDict[this.aspects[0]] = '';
     }
   }
 }
