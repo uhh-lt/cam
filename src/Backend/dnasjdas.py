62c4e178af00c5f9c2853006043a0e0811618716
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-03-02 16:37:28 +0100

    Removed test  file

--- src/Backend/dnasjdas.py
+++
@@ -1,6 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-
-wordlist = word_tokenize('This \'is a . test; some-thing')
-wordlist = [word for word in wordlist if len(word) > 1]
-print(wordlist)
