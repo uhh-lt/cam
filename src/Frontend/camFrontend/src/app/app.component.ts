ef6a6b03ef1aa850f3f127b536ce812fa559b5f0
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-03-07 16:31:54 +0100

    clusterer, method extracts

--- src/Frontend/camFrontend/src/app/app.component.ts
+++ src/Frontend/camFrontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // needed for the http.get method
 import { UrlBuilderComponent } from './url-builder/url-builder.component';
+import { ClustererComponent } from './clusterer/clusterer.component';
 
 /**
  * UI for the Comparative Argument Machine. Currently everything is done by this one class --
@@ -33,11 +34,13 @@ export class AppComponent {
   loser_links = []; // stores the main links of the second object
   winner_sentex = {}; // stores some example sentences for the first object
   loser_sentex = {}; // stores some example sentences for the second object
+  // sentences beyond the first to be shown for each object
   sentence_show_numberlist = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   constructor(
     private http: HttpClient,
-    private urlbuilder: UrlBuilderComponent
+    private urlbuilder: UrlBuilderComponent,
+    private clusterer: ClustererComponent
   ) {}
 
   /**
@@ -45,15 +48,17 @@ export class AppComponent {
    */
   compare() {
     this.loadshow = true; // show the loading screen
-    // reset everything to its default and hide the result table
-    this.reset();
+    this.reset(); // reset everything to its default and hide the result table
     // read the aspects entered by the user and store them with their weight
     for (const aspect of this.aspects) {
       if (this.aspectDict[aspect] !== undefined) {
-        this.finalAspDict[this.aspectDict[aspect]] = this.weightDict[aspect];
+        this.finalAspDict[this.aspectDict[aspect].trim()] = this.weightDict[
+          aspect
+        ];
       }
     }
     // read the objects entered, build the URL and start the search request
+    this.saveObjects();
     this.http
       .get(
         this.urlbuilder.buildURL(
@@ -89,10 +94,29 @@ export class AppComponent {
    */
   saveResult(result) {
     this.resshow = true; // show the result table
+    // count the number of sentences used for comparison
     this.rescount =
       result['object 1 sentences'].length + result['object 2 sentences'].length;
-    const a_won = result['score object 1'] > result['score object 2'];
-    // save the winner
+    const a_won = result['score object 1'] > result['score object 2']; // did object A win?
+    this.saveWinner(result, a_won);
+    this.saveScores(result, a_won);
+    this.saveLinkedWords(result, a_won);
+    this.saveSentences(result, a_won);
+    this.loadshow = false; // hide the loading screen
+  }
+
+  saveObjects() {
+    this.object_A = this.object_A.trim();
+    this.object_B = this.object_B.trim();
+  }
+
+  /**
+   * Save the winner and loser of the comparison.
+   *
+   * @param result search result of Elastic Search
+   * @param a_won did object A win?
+   */
+  saveWinner(result, a_won) {
     if (a_won) {
       this.winner_obj = result['object 1'];
       this.loser_obj = result['object 2'];
@@ -100,109 +124,117 @@ export class AppComponent {
       this.winner_obj = result['object 2'];
       this.loser_obj = result['object 1'];
     }
-    // save the scores
-    if (a_won) {
-      this.winner_score = (
-        result['score object 1'] /
-        (result['score object 1'] + result['score object 2']) *
-        100
-      ).toFixed(2);
-      this.loser_score = (
-        result['score object 2'] /
-        (result['score object 1'] + result['score object 2']) *
-        100
-      ).toFixed(2);
-    } else {
-      this.winner_score = (
-        result['score object 2'] /
-        (result['score object 1'] + result['score object 2']) *
-        100
-      ).toFixed(2);
-      this.loser_score = (
-        result['score object 1'] /
-        (result['score object 1'] + result['score object 2']) *
-        100
-      ).toFixed(2);
-    }
-    // sort the main links for both objects by their values and save them
-    if (a_won) {
-      for (const link of result['main links object 1']) {
-        this.winner_links.push(link);
-      }
-      for (const link of result['main links object 2']) {
-        this.loser_links.push(link);
-      }
-    } else {
-      for (const link of result['main links object 1']) {
-        this.loser_links.push(link);
-      }
-      for (const link of result['main links object 2']) {
-        this.winner_links.push(link);
-      }
-    }
-    // save the sentences each of the objects has won
-    let i = 0;
-    if (a_won) {
-      for (const sentence of result['object 1 sentences']) {
-        this.winner_sentex[i++] = this.getCluster(sentence);
-      }
-      i = 0;
-      for (const sentence of result['object 2 sentences']) {
-        this.loser_sentex[i++] = this.getCluster(sentence);
-      }
-    } else {
-      for (const sentence of result['object 1 sentences']) {
-        this.loser_sentex[i++] = this.getCluster(sentence);
-      }
-      i = 0;
-      for (const sentence of result['object 2 sentences']) {
-        this.winner_sentex[i++] = this.getCluster(sentence);
-      }
-    }
-    this.loadshow = false; // hide the loading screen
   }
 
-  getCluster(sentence) {
-    const wordList = sentence.match(/([A-Za-z]+)/g);
-    const highlightList = this.winner_links
-      .concat(this.loser_links)
-      .concat(Object.keys(this.finalAspDict));
-    highlightList.push(this.object_A);
-    highlightList.push(this.object_B);
-    const retDict = {};
-    let i = 0;
-    for (const word of wordList) {
-      if (highlightList.includes(word)) {
-        retDict[i] = { noHL: '' };
-        if (this.winner_links.includes(word)) {
-          retDict[i++] = { link: [word] };
-          continue;
-        }
-        retDict[i] = { link: '' };
-        if (this.loser_links.includes(word)) {
-          retDict[i++] = { link: [word] };
-          continue;
-        }
-        retDict[i] = { link: '' };
-        if (Object.keys(this.finalAspDict).includes(word)) {
-          retDict[i++] = { aspect: [word] };
-          continue;
-        }
-        retDict[i] = { aspect: '' };
-        if (word === this.object_A) {
-          retDict[i++] = { winner: [word] };
-          continue;
-        }
-        retDict[i] = { winner: '' };
-        retDict[i++] = { loser: [word] };
-        continue;
-      }
-      retDict[i++] = { noHL: [word] };
+  /**
+   * Save the percentage of the scores for each object.
+   *
+   * @param result search result of Elastic Search
+   * @param a_won did object A win?
+   */
+  saveScores(result, a_won) {
+    if (a_won) {
+      this.winner_score = (
+        result['score object 1'] /
+        (result['score object 1'] + result['score object 2']) *
+        100
+      ).toFixed(2);
+      this.loser_score = (
+        result['score object 2'] /
+        (result['score object 1'] + result['score object 2']) *
+        100
+      ).toFixed(2);
+    } else {
+      this.winner_score = (
+        result['score object 2'] /
+        (result['score object 1'] + result['score object 2']) *
+        100
+      ).toFixed(2);
+      this.loser_score = (
+        result['score object 1'] /
+        (result['score object 1'] + result['score object 2']) *
+        100
+      ).toFixed(2);
     }
-    const retList = [];
-    retList.push(retDict);
-    retList.push(Array.from(Array(wordList.length).keys()));
-    return retList;
+  }
+
+  /**
+   * Save the words linked to each object.
+   *
+   * @param result search result of Elastic Search
+   * @param a_won did object A win?
+   */
+  saveLinkedWords(result, a_won) {
+    if (a_won) {
+      for (const link of result['main links object 1']) {
+        this.winner_links.push(link);
+      }
+      for (const link of result['main links object 2']) {
+        this.loser_links.push(link);
+      }
+    } else {
+      for (const link of result['main links object 1']) {
+        this.loser_links.push(link);
+      }
+      for (const link of result['main links object 2']) {
+        this.winner_links.push(link);
+      }
+    }
+  }
+
+  /**
+   * Save the clustered sentences each object has won.
+   *
+   * @param result search result of Elastic Search
+   * @param a_won did object A win?
+   */
+  saveSentences(result, a_won) {
+    let i = 0;
+    if (a_won) {
+      for (const sentence of result['object 1 sentences']) {
+        this.winner_sentex[i++] = this.clusterer.getCluster(
+          sentence,
+          this.winner_links,
+          this.loser_links,
+          this.finalAspDict,
+          this.object_A,
+          this.object_B
+        );
+      }
+      i = 0;
+      for (const sentence of result['object 2 sentences']) {
+        this.loser_sentex[i++] = this.clusterer.getCluster(
+          sentence,
+          this.winner_links,
+          this.loser_links,
+          this.finalAspDict,
+          this.object_A,
+          this.object_B
+        );
+      }
+    } else {
+      for (const sentence of result['object 1 sentences']) {
+        this.loser_sentex[i++] = this.clusterer.getCluster(
+          sentence,
+          this.winner_links,
+          this.loser_links,
+          this.finalAspDict,
+          this.object_A,
+          this.object_B
+        );
+      }
+      i = 0;
+      for (const sentence of result['object 2 sentences']) {
+        this.winner_sentex[i++] = this.clusterer.getCluster(
+          sentence,
+          this.winner_links,
+          this.loser_links,
+          this.finalAspDict,
+          this.object_A,
+          this.object_B
+        );
+      }
+    }
   }
 
   /**
@@ -214,6 +246,10 @@ export class AppComponent {
     return this.object_A !== '' && this.object_B !== '';
   }
 
+  /**
+   * Calls compare() if the user entered something in both the first and the second object fields.
+   *
+   */
   compIfEntered() {
     if (this.objectsEntered()) {
       this.compare();
@@ -221,7 +257,7 @@ export class AppComponent {
   }
 
   /**
-   * Adds an aspect to the list of currently shown aspects which makes the UI show a new aspect row
+   * Adds an aspect to the list of currently shown aspects.
    *
    */
   addAspect() {
@@ -246,6 +282,10 @@ export class AppComponent {
     }
   }
 
+  /**
+   * Shows 10 more sentences in the result table.
+   *
+   */
   show_more_sentences() {
     let i = 0;
     while (i < 10) {
