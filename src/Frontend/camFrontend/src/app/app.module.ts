3758ab6d45f82b652d5b436735b5072816109a6b
ChulioZ <36635629+ChulioZ@users.noreply.github.com>
2018-03-07 15:24:05 +0100

    added clusterer

--- src/Frontend/camFrontend/src/app/app.module.ts
+++ src/Frontend/camFrontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { UrlBuilderComponent } from './url-builder/url-builder.component';
+import { AppComponent } from "./app.component";
+import { UrlBuilderComponent } from "./url-builder/url-builder.component";
+import { ClustererComponent } from "./clusterer/clusterer.component";
 
 @NgModule({
-  declarations: [AppComponent, UrlBuilderComponent],
+  declarations: [AppComponent, UrlBuilderComponent, ClustererComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [UrlBuilderComponent],
+  providers: [UrlBuilderComponent, ClustererComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
