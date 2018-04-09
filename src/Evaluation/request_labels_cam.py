9f5a81a08eb0d608c601395ac87722cc70735f3b
mschild <matthiass2006@web.de>
2018-04-09 18:45:25 +0200

    added aspect extracting

--- src/Evaluation/request_labels_cam.py
+++ src/Evaluation/request_labels_cam.py
@@ -3,6 +3,7 @@ import csv
 import operator
 import time
 
+
 def buildURL(objA, objB, aspectMap, model, fastSearch):
     hostname = ''
     if model == 'default':
@@ -18,7 +19,8 @@ def addAspectURL(aspectMap):
     url_part = ''
     i = 1
     for k, v in aspectMap.items():
-        url_part += '&aspect' + i + '=' + k + '&weight' + i+1 + '=' + v
+        url_part += '&aspect' + str(i) + '=' + k + \
+            '&weight' + str(i+1) + '=' + str(v)
         i += 1
     return url_part
 
@@ -28,8 +30,9 @@ def requestLabels(url, objects):
 
     scoreA = jsonResult.json()['score object 1']
     scoreB = jsonResult.json()['score object 2']
-   
+
     return [objects[0], objects[1], scoreA, scoreB]
+
 
 def main():
     pairs = []
@@ -41,15 +44,26 @@ def main():
 
     requested = [['object_a', 'object_b', 'score_a', 'score_b']]
 
+    i = 0
     for objects in pairs:
-        url = buildURL(objects[0], objects[1], dict(), 'default', 'false')
+        aspects = objects[2:]
+        aspectDict = {}
+        for aspect in aspects:
+            aspectDict[aspect] = 5
+        print(i)
+        i += 1
+        print(aspectDict)
+        url = buildURL(objects[0], objects[1], aspectDict, 'default', 'false')
+        print(url)
         result = requestLabels(url, objects)
-        print(result[0] + ' ' +  result[1] + ' ' + str(result[2]) + ' ' + str(result[3]))
+        print(result[0] + ' ' + result[1] + ' ' +
+              str(result[2]) + ' ' + str(result[3]))
         requested.append(result)
 
     with open('./csv/requested_labels.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(requested)
+
 
 start_time = time.time()
 main()
