8b32af7fa41139869ae6347cd97b9e2d7bd82a11
mschild <matthiass2006@web.de>
2018-04-11 10:15:58 +0200

    now producing aspects per sentence

--- src/Evaluation/preprocessing_dataset.py
+++ src/Evaluation/preprocessing_dataset.py
@@ -29,108 +29,52 @@ def tag_sentence(sentence):
     taglist = nltk.pos_tag(wordlist)
     return taglist
 
-
-def collectAspectsPerTriple(objectList):
-    tripleSentences = {}
-    pairs = set()
-
-    for objects in objectList:
-        objectA = objects[0]
-        objectB = objects[1]
-        label = objects[2]
-        sentence = objects[3]
-
-        pair = objectA + ',' + objectB
-        inversePair = objectB + ',' + objectA
-        triple = pair + ',' + label
-        inverseTriple = inversePair + ',' + buildInverseLabel(label)
-
-        aspects = generateAspects(sentence, objectA, objectB)
-
-        if triple in tripleSentences:
-            tripleSentences[triple].update(aspects)
-        elif inverseTriple in tripleSentences:
-            tripleSentences[inverseTriple].update(aspects)
-        elif pair in pairs:
-            tripleSentences[triple] = set(aspects)
-        elif inversePair in pairs:
-            tripleSentences[inverseTriple] = set(aspects)
-        else:
-            pairs.add(pair)
-            tripleSentences[triple] = set(aspects)
-
-    return [tripleSentences, pairs]
-
-
-def filterCommonAspects(listA, listB, listC):
-    listA = list(set(listA).difference(listB))
-    return list(set(listA).difference(listC))
-
-
-def buildInverseLabel(label):
-    if label == 'BETTER':
-        label = 'WORSE'
-    elif label == 'WORSE':
-        label = 'BETTER'
-    return label
-
-
 def generateAspects(sentence, objectA, objectB):
     taglist = tag_sentence(sentence)
-    aspects = []
+    aspects = set()
     for tag in taglist:
         possibleAspect = tag[0].lower()
         if tag[1].startswith('NN') and possibleAspect != objectA and possibleAspect != objectB:
-            aspects.append(possibleAspect)
+            aspects.add(possibleAspect)
 
     return aspects
 
-
-def main():
-    objectList = extractData()
-
-    triplesAndPairs = collectAspectsPerTriple(objectList)
-    tripleAspects = triplesAndPairs[0]
-    pairs = triplesAndPairs[1]
-
-    print(len(pairs))
-    for pair in pairs:
-        betterTriple = pair + ',BETTER'
-        worseTriple = pair + ',WORSE'
-        noneTriple = pair + ',NONE'
-        betterAspects = tripleAspects[betterTriple] if betterTriple in tripleAspects else []
-        worseAspects = tripleAspects[worseTriple] if worseTriple in tripleAspects else []
-        noneAspects = tripleAspects[noneTriple] if noneTriple in tripleAspects else []
-
-        if betterTriple in tripleAspects:
-            tripleAspects[betterTriple]  = filterCommonAspects(betterAspects, worseAspects, noneAspects)
-        if worseTriple in tripleAspects:
-            tripleAspects[worseTriple] = filterCommonAspects(worseAspects, betterAspects, noneAspects)
-        if noneTriple in tripleAspects:
-            tripleAspects[noneTriple] = filterCommonAspects(noneAspects, worseAspects, betterAspects)
-
-    triples = []
+def collectAspectsPerSentence(comparisonList):
     header = ['object_a', 'object_b', 'label']
+    sentences = []
     maxAspects = 0
-    for triple, aspects in tripleAspects.items():
+    for comparation in comparisonList:
+        objectA = comparation[0]
+        objectB = comparation[1]
+        label = comparation[2]
+        sentence = comparation[3]
+
+        sentences.append([objectA, objectB, label])
+        aspects = generateAspects(sentence, objectA, objectB)
+
         numberOfAspects = len(aspects)
-        # if numberOfAspects == 0:      # Filter out trible with no aspects
-        #     print(triple)
-        #     continue
         if maxAspects < numberOfAspects:
             maxAspects = numberOfAspects
-        triples.append([x for x in triple.split(',')])
-        triples[len(triples)-1].extend(aspects)
+
+        sentences[len(sentences)-1].extend(aspects)
 
     for i in range(0, maxAspects, 1):
         header.append('aspect_' + str(i))
 
-    triples.sort(key=operator.itemgetter(0, 1, 2))
-    triples.insert(0, header)
+    sentences.sort(key=operator.itemgetter(0, 1, 2))
+    sentences.insert(0, header)
+    return sentences
+    
+
+
+def main():
+    comparisonList = extractData()
+    comparationsWithAspects = collectAspectsPerSentence(comparisonList)
+
 
     with open('./csv/preprocessed_dataset.csv', 'w', newline='', encoding="UTF-8") as f:
         writer = csv.writer(f)
-        writer.writerows(triples)
+        writer.writerows(comparationsWithAspects)
 
 
 start_time = time.time()
