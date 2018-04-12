c06411eaf0787cfa58fc9febeb00d693b0e7b471
mschild <matthiass2006@web.de>
2018-04-12 12:28:27 +0200

    added routing

--- src/Frontend/camFrontend/src/app/app.module.ts
+++ src/Frontend/camFrontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -9,10 +10,35 @@ import { UrlBuilderService } from './shared/url-builder.service';
 import { ClustererService } from './shared/clusterer.service';
 import { HTTPRequestService } from './shared/http-request.service';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ApiInfoComponent } from './components/api-info/api-info.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { UserInterfaceComponent } from './components/user-interface/user-interface.component';
+
+
+const appRoute: Routes = [
+  { path: '', component: UserInterfaceComponent},
+  { path: 'About', component: AboutComponent},
+  { path: 'API-Info', component: ApiInfoComponent},
+  { path: 'Contact', component: ContactComponent}
+
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ApiInfoComponent,
+    ContactComponent,
+    UserInterfaceComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoute)
+  ],
   providers: [UrlBuilderService, ClustererService, HTTPRequestService],
   bootstrap: [AppComponent]
 })
---
+++ src/Frontend/camFrontend/src/app/components/api-info/api-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-api-info',
+  templateUrl: './api-info.component.html',
+  styleUrls: ['./api-info.component.css']
+})
+export class ApiInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
