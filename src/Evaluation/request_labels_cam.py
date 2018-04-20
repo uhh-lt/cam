beefae8c0837c4f96386e693e2ca8c58516ada05
mschild <matthiass2006@web.de>
2018-04-20 19:06:50 +0200

    shortend thread creation

--- src/Evaluation/request_labels_cam.py
+++ src/Evaluation/request_labels_cam.py
@@ -3,6 +3,7 @@ import csv
 import operator
 import time
 import threading
+import math
 
 
 class myThread (threading.Thread):
@@ -48,8 +49,10 @@ def buildURL(objA, objB, aspectMap, model, fastSearch):
     hostname = ''
     if model == 'default':
         hostname = 'http://ltdemos.informatik.uni-hamburg.de/cam-api'
+        # hostname = 'http://127.0.0.1:5000/cam'
     elif model == 'machine_learning':
         hostname = 'http://ltdemos.informatik.uni-hamburg.de/cam-api'
+        # hostname = 'http://127.0.0.1:5000/cam'
     URL = hostname + '?fs=' + fastSearch + '&objectA=' + objA + '&objectB=' + objB
     URL += addAspectURL(aspectMap)
     return URL
@@ -91,82 +94,23 @@ def main():
 
     # *****Threads here*****
     # Create new threads
-    threads = []
 
-    i = 10
-    thread1 = myThread(1, "Thread-1", comparations[0:i])
-    thread2 = myThread(2, "Thread-2", comparations[i:2*i])
-    thread3 = myThread(3, "Thread-3", comparations[2*i:3*i])
-    thread4 = myThread(4, "Thread-4", comparations[3*i:4*i])
-    thread5 = myThread(5, "Thread-5", comparations[4*i:5*i])
-    thread6 = myThread(6, "Thread-6", comparations[5*i:6*i])
-    thread7 = myThread(7, "Thread-7", comparations[6*i:7*i])
-    thread8 = myThread(8, "Thread-8", comparations[7*i:8*i])
-    thread9 = myThread(9, "Thread-9", comparations[8*i:9*i])
-    thread10 = myThread(10, "Thread-10", comparations[9*i:10*i])
-    thread11 = myThread(11, "Thread-11", comparations[10*i:11*i])
-    thread12 = myThread(12, "Thread-12", comparations[11*i:12*i])
-    thread13 = myThread(13, "Thread-13", comparations[12*i:13*i])
-    thread14 = myThread(14, "Thread-14", comparations[13*i:14*i])
-    thread15 = myThread(15, "Thread-15", comparations[14*i:15*i])
-    thread16 = myThread(16, "Thread-16", comparations[15*i:16*i])
-    thread17 = myThread(17, "Thread-17", comparations[16*i:17*i])
-    thread18 = myThread(18, "Thread-18", comparations[17*i:18*i])
-    thread19 = myThread(19, "Thread-19", comparations[18*i:19*i])
-    thread20 = myThread(20, "Thread-20", comparations[19*i:20*i])
+    numberOfThreads = 20
+    i = math.ceil(len(comparations)/numberOfThreads)
+    threads = []
+    for j in range(0, numberOfThreads, 1):
+        threads.append(myThread(j, "Thread-" + str(j),
+                                comparations[(j*i):((j+1)*i)]))
 
     # Start new Threads
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-    thread6.start()
-    thread7.start()
-    thread8.start()
-    thread9.start()
-    thread10.start()
-    thread11.start()
-    thread12.start()
-    thread13.start()
-    thread14.start()
-    thread15.start()
-    thread16.start()
-    thread17.start()
-    thread18.start()
-    thread19.start()
-    thread20.start()
-
-    # Add threads to thread list
-    threads.append(thread1)
-    threads.append(thread2)
-    threads.append(thread3)
-    threads.append(thread4)
-    threads.append(thread5)
-    threads.append(thread6)
-    threads.append(thread7)
-    threads.append(thread8)
-    threads.append(thread9)
-    threads.append(thread10)
-    threads.append(thread11)
-    threads.append(thread12)
-    threads.append(thread13)
-    threads.append(thread14)
-    threads.append(thread15)
-    threads.append(thread16)
-    threads.append(thread17)
-    threads.append(thread18)
-    threads.append(thread19)
-    threads.append(thread20)
+    for t in threads:
+        t.start()
 
     print('Processing...')
     # Wait for all threads to complete
-    while(thread1.isAlive() or thread2.isAlive() or thread3.isAlive() or thread4.isAlive()
-          or thread5.isAlive() or thread6.isAlive() or thread7.isAlive() or thread8.isAlive()
-          or thread9.isAlive() or thread10.isAlive() or thread11.isAlive() or thread12.isAlive()
-          or thread13.isAlive() or thread14.isAlive() or thread15.isAlive() or thread16.isAlive()
-          or thread17.isAlive() or thread18.isAlive() or thread19.isAlive() or thread20.isAlive()):
-        pass
+    for t in threads:
+        while(t.isAlive()):
+            pass
 
     for t in threads:
         print('Wait')
@@ -177,7 +121,7 @@ def main():
     print(str(len(requested)) + ' results.')
     # **********************
 
-    with open('./csv/({})_requested_labels.csv'.format(urlParam), 'w', newline='') as f:
+    with open('./csv/({})_requested_labels.csv'.format(urlParam), 'w', newline='', encoding="UTF-8") as f:
         writer = csv.writer(f)
         writer.writerows(requested)
 
