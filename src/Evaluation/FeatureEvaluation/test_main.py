1387a7d0225220e9469d43b4d994c5880be16b00
mschild <matthiass2006@web.de>
2018-07-19 12:20:12 +0200

    refactoring for eva setup

---
+++ src/Evaluation/FeatureEvaluation/test_main.py
@@ -0,0 +1,106 @@
+import csv
+import sys
+sys.path.append('../../Backend')
+from utils.es_requester import request_es, extract_sentences, request_es_ML, request_es_triple
+from utils.sentence_clearer import clear_sentences, remove_questions
+from utils.summarization import find_most_frequent_words
+from ml_approach.sentence_preparation_ML import prepare_sentence_DF
+from ml_approach.classify import classify_sentences, evaluate
+from marker_approach.object_comparer import find_winner
+from main import Argument, Aspect
+
+from score_determination import calculateEvaluationScores, set_counts, Scores
+from evaluation_triples import gold_scores, triples
+
+
+def simulate_main(triple):
+    fast_search = 'true'
+    obj_a = Argument(triple[0].lower().strip())
+    obj_b = Argument(triple[1].lower().strip())
+    aspects = [Aspect(triple[2].lower(), 5)]
+    model = 'bow'
+
+    if aspects:
+        json_compl_triples = request_es_triple(obj_a, obj_b, aspects)
+    json_compl = request_es_ML(fast_search, obj_a, obj_b)
+
+    if aspects:
+        all_sentences = extract_sentences(json_compl_triples)
+        all_sentences.update(extract_sentences(json_compl))
+    else:
+        all_sentences = extract_sentences(json_compl)
+
+    remove_questions(all_sentences)
+
+    prepared_sentences = prepare_sentence_DF(all_sentences, obj_a, obj_b)
+
+    classification_results = classify_sentences(prepared_sentences, model)
+
+    final_dict = evaluate(all_sentences, prepared_sentences,
+                          classification_results, obj_a, obj_b, aspects)
+
+    a_aspect_score = 0
+    if triple[2] in final_dict['scoreObject1']:
+        a_aspect_score = final_dict['scoreObject1'][triple[2]]
+    b_aspect_score = 0
+    if triple[2] in final_dict['scoreObject2']:
+        b_aspect_score = final_dict['scoreObject2'][triple[2]]
+    
+    return [a_aspect_score, b_aspect_score]
+
+
+def main():
+    index = 1
+    scores = [['index', 'a_score', 'b_score', 'gold_deviation']]
+    score_counts = Scores()
+    for triple in triples:
+
+        temp_scores = simulate_main(triple)
+        a_aspect_score = temp_scores[0]
+        b_aspect_score = temp_scores[1]
+
+        total_aspect_score = a_aspect_score + b_aspect_score
+
+        if total_aspect_score == 0:
+            print('Nothing found for: ', index,
+                  triple[0], triple[1], triple[2])
+            scores.append([index, 0, 0, ''])
+        else:
+            gold = gold_scores[index-1]
+            a_percent = (a_aspect_score / total_aspect_score)
+            b_percent = (b_aspect_score / total_aspect_score)
+            print(index, triple[0] + ':', a_percent, triple[1] + ':', b_percent, triple[2])
+            deviation = a_percent - gold if a_percent > gold else gold - a_percent
+            set_counts((a_aspect_score / total_aspect_score), gold, deviation, score_counts)
+
+            scores.append([index, a_percent, b_percent, deviation])
+        index = index + 1
+
+    summed_deviation = 0
+    total = len(scores)
+    for score in scores:
+        if score[3] == '' or score[0] == 'index':
+            total = total - 1
+            continue
+        summed_deviation = summed_deviation + float(score[3])
+
+    print('Average deviation:', summed_deviation / total, 'Incorrect:', score_counts.totalWrong)
+    totalPercentRight = round(score_counts.totalRight * 100 / (score_counts.totalRight + score_counts.totalWrong), 2)
+    evaluation_scores = [['label', 'precision', 'recall', 'f_1', 'accuracy', 'total accuracy']]
+    evaluation_scores.append(calculateEvaluationScores('BETTER', score_counts.betterTP, score_counts.betterTN, score_counts.betterFP, score_counts.betterFN, 2, ''))
+    evaluation_scores.append(calculateEvaluationScores('WORSE', score_counts.worseTP, score_counts.worseTN, score_counts.worseFP, score_counts.worseFN, 2, ''))
+    evaluation_scores.append(calculateEvaluationScores('NONE', score_counts.noneTP, score_counts.noneTN, score_counts.noneFP, score_counts.noneFN, 2, totalPercentRight))
+    with open('./eva_scores.csv', 'w', newline='', encoding="UTF-8") as f:
+        writer = csv.writer(f)
+        writer.writerows([list(x) for x in zip(*evaluation_scores)])
+
+    scores.append(['', '', 'ava. deviation:', summed_deviation/total])
+    scores.append(['', '', 'Incorrect:', score_counts.totalWrong])
+
+    with open('./aspect_scores.csv', 'w', newline='', encoding="UTF-8") as f:
+        writer = csv.writer(f)
+        writer.writerows(scores)
+
+
+if __name__ == "__main__":
+    main()
