f1ffdbc75476dc75090711f0e1c51424b72326fd
Julian Zenker <j.zenker@gmx.net>
2018-12-06 17:07:46 +0100

    wip

--- src/Backend/db/mysql_connecter.py
+++ src/Backend/db/mysql_connecter.py
@@ -62,9 +62,7 @@ def get_connection():
     try:
         cursor.execute("USE {}".format(DB_NAME))
     except:
-        print("Database {} does not exists.".format(DB_NAME))
         create_database(connection)
-        print("Database {} created successfully.".format(DB_NAME))
         connection.database = DB_NAME
         create_table(connection, create_ratings_table_sql)
         create_table(connection, create_pairs_table_sql)
@@ -77,43 +75,38 @@ def create_database(connection):
     try:
         cursor.execute(
             "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
+        connection.commit()
+        cursor.close()
+    except:
         exit(1)
 
 
 def create_table(connection, sql_command):
     cursor = connection.cursor()
-    try:
-        print("Creating table")
-        cursor.execute(sql_command)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-            print("already exists.")
-        else:
-            print(err.msg)
-    else:
-        print("OK")
+    cursor.execute(sql_command)
+    connection.commit()
+    cursor.close()
 
 
 def insert_predefined_pairs(connection, predefined_pairs):
     cursor = connection.cursor()
     for pair in predefined_pairs:
         pair.sort()
-        cursor.execute('INSERT INTO pairs VALUES (%s,%s,0)', pair)
-    close_connection(connection, cursor)
+        cursor.execute("INSERT INTO `pairs` VALUES (%s,%s,0)", pair)
+    connection.commit()
+    cursor.close()
 
 
 def get_predefined_pairs():
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM pairs')
+    cursor.execute("SELECT * FROM `pairs`")
     return cursor.fetchall()
 
 
 def insert_rating(rating: Rating):
     connection, cursor = get_connection()
-    cursor.execute('INSERT INTO ratings VALUES (%s,%s,%s,%s)',
+    cursor.execute("INSERT INTO `ratings` VALUES (%s,%s,%s,%s)",
                    rating.get_value())
     raise_value_of_pair(rating.get_pair(), cursor)
     close_connection(connection, cursor)
@@ -121,7 +114,7 @@ def insert_rating(rating: Rating):
 
 def raise_value_of_pair(pair, cursor):
     pair.sort()
-    cursor.execute('UPDATE pairs SET amount = amount + 1 WHERE obja = %s AND objb = %s', pair)
+    cursor.execute("UPDATE pairs SET amount = amount + 1 WHERE obja = %s AND objb = %s", pair)
 
 
 def close_connection(connection, cursor):
