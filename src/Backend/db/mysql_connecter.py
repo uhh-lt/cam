ca9d37b4019aeb0d17e8f7ea56934ad262e27462
Julian Zenker <j.zenker@gmx.net>
2018-12-09 10:54:26 +0100

    Convert exported ratings

--- src/Backend/db/mysql_connecter.py
+++ src/Backend/db/mysql_connecter.py
@@ -2,6 +2,7 @@ from os.path import abspath, dirname
 
 import mysql.connector
 from mysql.connector import errorcode
+import numpy
 
 pre_selected_objects = [
     ['python', 'java'],
@@ -36,6 +37,10 @@ create_pairs_table_sql = ("CREATE TABLE `pairs` ("
                           " `amount` int NOT NULL,"
                           " PRIMARY KEY (`obja`, `objb`)"
                           ") ENGINE=InnoDB")
+
+TARGET_DIR = dirname(dirname(dirname(dirname(abspath(__file__)))))
+RATINGS_FILE_NAME = TARGET_DIR + '/ratingresults/ratings.csv'
+CONVERTED_RATINGS_FILE_NAME = TARGET_DIR + '/ratingresults/convertedratings.csv'
 
 
 class Rating:
@@ -123,10 +128,50 @@ def raise_value_of_pair(pair, cursor):
 
 
 def export_rating(rating: Rating):
-    target_dir = dirname(dirname(dirname(dirname(abspath(__file__)))))
-    with open(target_dir + '/ratingresults/ratings.csv', 'a') as target_file:
+    with open(RATINGS_FILE_NAME, 'a') as target_file:
         target_file.write(';'.join([str(col) for col in rating.get_value()]) + '\n')
 
+
+def convert_exported_ratings():
+    rating_dict = {}
+    with open(RATINGS_FILE_NAME, 'r') as source_file:
+        for line in source_file:
+            aspect, rating_str, pair_str = line.split(';', 2)
+            pair = pair_str.replace(';', '/')
+            rating = int(rating_str)
+            if pair not in rating_dict.keys():
+                rating_dict[pair] = {}
+            if aspect not in rating_dict[pair].keys():
+                rating_dict[pair][aspect] = []
+            rating_dict[pair][aspect].append(rating)
+
+    with open(CONVERTED_RATINGS_FILE_NAME, 'w') as target_file:
+        for pair in rating_dict.keys():
+            rating_list = rating_dict[pair][aspect]
+            good_dict = {}
+            for aspect in rating_dict[pair].keys():
+                good_dict[aspect] = rating_list.count(1)
+            bad_dict = {}
+            for aspect in rating_dict[pair].keys():
+                bad_dict[aspect] = rating_list.count(0)
+            average_dict = {}
+            for aspect in rating_dict[pair].keys():
+                average_dict[aspect] = numpy.mean(rating_list)
+
+            target_file.write('aspects with the most GOOD ratings for ' + pair + ':\n')
+            for aspect in sorted(good_dict, key=good_dict.__getitem__, reverse=True):
+                target_file.write(aspect + ';' + good_dict[aspect] + '\n')
+            target_file.write('\n\n')
+
+            target_file.write('aspects with the most BAD ratings for ' + pair + ':\n')
+            for aspect in sorted(bad_dict, key=bad_dict.__getitem__, reverse=True):
+                target_file.write(aspect + ';' + bad_dict[aspect] + '\n')
+            target_file.write('\n\n')
+
+            target_file.write('average aspect ratings for ' + pair + ':\n')
+            for aspect in sorted(average_dict, key=average_dict.__getitem__, reverse=True):
+                target_file.write(aspect + ';' + average_dict[aspect] + '\n')
+            target_file.write('\n\n')
 
 def close_connection(connection, cursor):
     connection.commit()
