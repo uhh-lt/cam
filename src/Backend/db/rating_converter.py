64f1c8a6962f74f4187d4f9d5749c7aac93dbe78
Julian Zenker <j.zenker@gmx.net>
2018-12-09 11:06:21 +0100

    Convert exported ratings

--- src/Backend/db/rating_converter.py
+++ src/Backend/db/rating_converter.py
@@ -20,17 +20,16 @@ def convert_exported_ratings():
 
     with open(CONVERTED_RATINGS_FILE_NAME, 'w') as target_file:
         for pair in rating_dict.keys():
-            rating_list = rating_dict[pair][aspect]
             good_dict = {}
             for aspect in rating_dict[pair].keys():
-                good_dict[aspect] = rating_list.count(1)
+                good_dict[aspect] = rating_dict[pair][aspect].count(1)
             bad_dict = {}
             for aspect in rating_dict[pair].keys():
-                bad_dict[aspect] = rating_list.count(0)
+                bad_dict[aspect] = rating_dict[pair][aspect].count(0)
             average_dict = {}
             for aspect in rating_dict[pair].keys():
                 average_dict[aspect] = sum(
-                    rating_list) / float(len(rating_list))
+                    rating_dict[pair][aspect]) / float(len(rating_dict[pair][aspect]))
 
             target_file.write(
                 'aspects with the most GOOD ratings for ' + pair + ':\n')
@@ -46,5 +45,6 @@ def convert_exported_ratings():
 
             target_file.write('average aspect ratings for ' + pair + ':\n')
             for aspect in sorted(average_dict, key=average_dict.__getitem__, reverse=True):
-                target_file.write(aspect + ';' + str(average_dict[aspect]) + '\n')
+                target_file.write(
+                    aspect + ';' + str(average_dict[aspect]) + '\n')
             target_file.write('\n\n')
