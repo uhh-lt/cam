559f70ecf6794ad6c80ee0ae1194e814afe076e5
Julian Zenker <j.zenker@gmx.net>
2018-12-27 13:30:11 +0100

    convert ratings to list

--- src/Backend/db/rating_converter.py
+++ src/Backend/db/rating_converter.py
@@ -1,10 +1,6 @@
 import random
 from os.path import abspath, dirname
 
-from utils.es_requester import extract_sentences, request_es
-from utils.objects import Argument
-from utils.sentence_clearer import clear_sentences
-
 TARGET_DIR = dirname(dirname(dirname(dirname(abspath(__file__)))))
 RATINGS_FILE_NAME = TARGET_DIR + '/ratingresults/ratings.csv'
 CONVERTED_RATINGS_FILE_NAME = TARGET_DIR + '/ratingresults/convertedratings.csv'
@@ -25,17 +21,10 @@ def convert_exported_ratings():
 
     with open(CONVERTED_RATINGS_FILE_NAME, 'w') as target_file:
         target_file.write(
-            'object_a;object_b;aspect;most_frequent_rating;confidence;good_ratings;bad_ratings;example_sentence_1;example_sentence_2;example_sentence_3')
+            'object_a;object_b;aspect;most_frequent_rating;confidence;good_ratings;bad_ratings')
 
         for pair in rating_dict.keys():
             object_a, object_b = pair.split('///', 1)
-
-            obj_a = Argument(object_a)
-            obj_b = Argument(object_b)
-
-            json_compl = request_es('false', obj_a, obj_b)
-            all_sentences = extract_sentences(json_compl)
-            all_sentences = clear_sentences(all_sentences, obj_a, obj_b)
 
             target_file.write(object_a + ';' + object_b + ';')
 
@@ -55,21 +44,4 @@ def convert_exported_ratings():
                         float(good_ratings + bad_ratings)
 
                 target_file.write(most_frequent_rating + ';' + str(confidence) +
-                                  ';' + str(good_ratings) + ';' + str(bad_ratings) + ';')
-
-                aspect_sentences = [
-                    sentence for sentence in all_sentences if aspect in sentence]
-                chosen_sentences = random.sample(
-                    aspect_sentences, min(3, len(aspect_sentences)))
-
-                example_sentence_1 = ''
-                example_sentence_2 = ''
-                example_sentence_3 = ''
-                if len(chosen_sentences) > 0:
-                    example_sentence_1 = chosen_sentences[0]
-                    if len(chosen_sentences) > 1:
-                        example_sentence_2 = chosen_sentences[1]
-                        if len(chosen_sentences) > 2:
-                            example_sentence_3 = chosen_sentences[2]
-                target_file.write(
-                    example_sentence_1 + ';' + example_sentence_2 + ';' + example_sentence_3 + '\n')
+                                  ';' + str(good_ratings) + ';' + str(bad_ratings) + '\n')
