ccb17a77bcd17d61022244f041fca4f51962bc87
Julian Zenker <j.zenker@gmx.net>
2018-12-27 13:13:28 +0100

    convert ratings to list

--- src/Backend/db/rating_converter.py
+++ src/Backend/db/rating_converter.py
@@ -1,4 +1,9 @@
+import random
 from os.path import abspath, dirname
+
+from utils.es_requester import extract_sentences, request_es
+from utils.objects import Argument
+from utils.sentence_clearer import clear_sentences
 
 TARGET_DIR = dirname(dirname(dirname(dirname(abspath(__file__)))))
 RATINGS_FILE_NAME = TARGET_DIR + '/ratingresults/ratings.csv'
@@ -10,7 +15,7 @@ def convert_exported_ratings():
     with open(RATINGS_FILE_NAME, 'r') as source_file:
         for line in source_file:
             aspect, rating_str, pair_str = line.split(';', 2)
-            pair = pair_str.replace(';', '/')
+            pair = pair_str.replace(';', '///')
             rating = int(rating_str)
             if pair not in rating_dict.keys():
                 rating_dict[pair] = {}
@@ -19,32 +24,52 @@ def convert_exported_ratings():
             rating_dict[pair][aspect].append(rating)
 
     with open(CONVERTED_RATINGS_FILE_NAME, 'w') as target_file:
+        target_file.write(
+            'object_a;object_b;aspect;most_frequent_rating;confidence;good_ratings;bad_ratings;example_sentence_1;example_sentence_2;example_sentence_3')
+
         for pair in rating_dict.keys():
-            good_dict = {}
-            for aspect in rating_dict[pair].keys():
-                good_dict[aspect] = rating_dict[pair][aspect].count(1)
-            bad_dict = {}
-            for aspect in rating_dict[pair].keys():
-                bad_dict[aspect] = rating_dict[pair][aspect].count(0)
-            average_dict = {}
-            for aspect in rating_dict[pair].keys():
-                average_dict[aspect] = sum(
-                    rating_dict[pair][aspect]) / float(len(rating_dict[pair][aspect]))
+            object_a, object_b = pair.split('///', 1)
 
-            target_file.write(
-                'aspects with the most GOOD ratings for ' + pair + ':\n')
-            for aspect in sorted(good_dict, key=good_dict.__getitem__, reverse=True):
-                target_file.write(aspect + ';' + str(good_dict[aspect]) + '\n')
-            target_file.write('\n\n')
+            obj_a = Argument(object_a)
+            obj_b = Argument(object_b)
 
-            target_file.write(
-                'aspects with the most BAD ratings for ' + pair + ':\n')
-            for aspect in sorted(bad_dict, key=bad_dict.__getitem__, reverse=True):
-                target_file.write(aspect + ';' + str(bad_dict[aspect]) + '\n')
-            target_file.write('\n\n')
+            json_compl = request_es('false', obj_a, obj_b)
+            all_sentences = extract_sentences(json_compl)
+            all_sentences = clear_sentences(all_sentences, obj_a, obj_b)
 
-            target_file.write('average aspect ratings for ' + pair + ':\n')
-            for aspect in sorted(average_dict, key=average_dict.__getitem__, reverse=True):
+            target_file.write(object_a + ';' + object_b + ';')
+
+            for aspect in rating_dict[pair].keys():
+                target_file.write(aspect + ';')
+
+                good_ratings = rating_dict[pair][aspect].count(1)
+                bad_ratings = rating_dict[pair][aspect].count(0)
+
+                if good_ratings > bad_ratings:
+                    most_frequent_rating = 'good'
+                    confidence = good_ratings / \
+                        float(good_ratings + bad_ratings)
+                else:
+                    most_frequent_rating = 'bad'
+                    confidence = bad_ratings / \
+                        float(good_ratings + bad_ratings)
+
+                target_file.write(most_frequent_rating + ';' + str(confidence) +
+                                  ';' + str(good_ratings) + ';' + str(bad_ratings) + ';')
+
+                aspect_sentences = [
+                    sentence for sentence in all_sentences if aspect in sentence]
+                chosen_sentences = random.sample(
+                    aspect_sentences, min(3, len(aspect_sentences)))
+
+                example_sentence_1 = ''
+                example_sentence_2 = ''
+                example_sentence_3 = ''
+                if len(chosen_sentences) > 0:
+                    example_sentence_1 = chosen_sentences[0]
+                    if len(chosen_sentences) > 1:
+                        example_sentence_2 = chosen_sentences[1]
+                        if len(chosen_sentences) > 2:
+                            example_sentence_3 = chosen_sentences[2]
                 target_file.write(
-                    aspect + ';' + str(average_dict[aspect]) + '\n')
-            target_file.write('\n\n')
+                    example_sentence_1 + ';' + example_sentence_2 + ';' + example_sentence_3 + '\n')
