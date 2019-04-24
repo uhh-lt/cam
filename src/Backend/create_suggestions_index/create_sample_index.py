d4e7830cc35089c590295df7dda088b1785d2af9
Hauke Heller <hauke.heller@gmail.com>
2019-04-24 12:41:38 +0200

    use python pool to compute suggestions for english words in parallel

--- src/Backend/create_suggestions_index/create_sample_index.py
+++ src/Backend/create_suggestions_index/create_sample_index.py
@@ -5,7 +5,7 @@ from sample_wordlist import comparison_objects
 from sample_wordlist import comparison_objects_small
 import requests
 import json
-
+from multiprocessing import Pool
 
 ES_HOST = {"host" : "localhost", "port" : 9200}
 INDEX_NAME = "suggestions-index"
@@ -31,25 +31,30 @@ print("creating '%s' index..." % (INDEX_NAME))
 res = es.indices.create(index = INDEX_NAME, body = request_body)
 print(" response: '%s'" % (res))
 
-total_time_start = time.time()
 
-index = 0
-start = time.time()
+# New version, parallel on server:
+
+
+
+
+# Old version, one by one, locally:
+#total_time_start = time.time()
+
+#index = 0
+#start = time.time()
 # do something with each word in the sample_wordlist
-for comparison_object in comparison_objects:
-    index += 1
-    data_dict = {}
-    ccr_suggestions = requests.get(CCR_BASE_URL + '{}'.format(comparison_object)).json()
-    data_dict[comparison_object] = ccr_suggestions
-
-    data = {}
-    data = {
-        "comparison_object": comparison_object,
-        "suggestions": ccr_suggestions
-        }
-
-
-    es.index(index = INDEX_NAME, doc_type = TYPE_NAME, id = index, body = data)
+#for comparison_object in comparison_objects:
+#    index += 1
+#    ccr_suggestions = requests.get(CCR_BASE_URL + '{}'.format(comparison_object)).json()
+#
+#    data = {}
+#    data = {
+#        "comparison_object": comparison_object,
+#        "suggestions": ccr_suggestions
+#        }
+#
+#
+#    es.index(index = INDEX_NAME, doc_type = TYPE_NAME, id = index, body = data)
 
 
 res1 = es.get(index = INDEX_NAME, doc_type = TYPE_NAME, id = 1)
