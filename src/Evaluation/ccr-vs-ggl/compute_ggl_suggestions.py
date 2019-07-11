d95c272061675f246ce5a8e776c3d179391da832
Hauke Heller <hauke.heller@gmail.com>
2019-07-11 16:28:14 +0200

    format ggl suggestions no longer necessary hence deleted

--- src/Evaluation/ccr-vs-ggl/compute_ggl_suggestions.py
+++ src/Evaluation/ccr-vs-ggl/compute_ggl_suggestions.py
@@ -2,25 +2,29 @@ from query_ggl_suggestions import get_suggestions
 import json
 
 
-co_ggl_suggestions_dict = []
+ggl_suggestions_array = []
 
 for file_counter in range(0,1,1):
     # https://github.com/dwyl/english-words
-    filename = '../../Backend/create_suggestions_index/english_words/wordlist-{}.json'.format(str('%05d' % file_counter))
+    filename = '../../Backend/create_suggestions_index/english_words/dict-B.json'
     with open(filename) as json_file:
         comparison_objects = json.load(json_file)
         for comparison_object in comparison_objects:
             print(comparison_object)
+            suggestionsArray = []
             ggl_suggestions = get_suggestions(comparison_object)
+            for suggestion in ggl_suggestions:
+                if (len(suggestion[0].split(None, 2)) > 2):
+                    suggestionsArray.append(suggestion[0].split(None, 2)[2])
             data = {
                 "comparison_object": comparison_object,
-                "suggestions": ggl_suggestions
+                "suggestions": suggestionsArray
             }
             print(ggl_suggestions)
-            co_ggl_suggestions_dict.append(data)
+            ggl_suggestions_array.append(data)
 
-    outfilename = './ggl-suggestions/ggl-outfile-{}.json'.format(str('%05d' % file_counter))
+    outfilename = './ggl-suggestions/dict-B.json'
     with open(outfilename, 'w') as outfile:
-        json.dump(co_ggl_suggestions_dict, outfile)
+        json.dump(ggl_suggestions_array, outfile)
 
 
--- src/Evaluation/ccr-vs-ggl/compute_ggl_suggestions_pool.py
+++ src/Evaluation/ccr-vs-ggl/compute_ggl_suggestions_pool.py
@@ -6,9 +6,13 @@ from query_ggl_suggestions import get_suggestions
 def requestGglSuggestions(comparison_object):
     ggl_suggestions = get_suggestions('{} vs'.format(comparison_object))
     print(ggl_suggestions)
+    for suggestion in ggl_suggestions:
+    if (len(suggestion[0].split(None, 2)) > 2):
+        suggestionsArray.append(suggestion[0].split(None, 2)[2])
+
     data = {
         "comparison_object": comparison_object,
-        "suggestions": ggl_suggestions
+        "suggestions": suggestionsArray
     }
     return data
 
@@ -21,7 +25,7 @@ with open(filename) as json_file:
     comparison_objects = json.load(json_file)
     co_ggl_suggestions_dict = p.map(requestGglSuggestions, comparison_objects)
 
-outfilename = './ggl-suggestions/cam-suggestions-dict-A.json'
+outfilename = './ggl-suggestions/ggl-suggestions-dict-A.json'
 with open(outfilename, 'w') as outfile:
     json.dump(co_ggl_suggestions_dict, outfile)
 
