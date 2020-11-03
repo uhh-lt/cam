084a8894f586cf52ef56d574100dd53badf302dd
Jan Heinrich Reimer <heinrich@reimer.family>
2020-11-03 12:52:50 +0100

    Make frontend URLs configurable

--- src/Frontend/camFrontend/src/environments/environment.prod.ts
+++ src/Frontend/camFrontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://ltdemos.informatik.uni-hamburg.de/cam-api',
+  suggestionsElasticsearchUrl: 'http://localhost:9200/suggestions-index/suggestions/_search'
 };
