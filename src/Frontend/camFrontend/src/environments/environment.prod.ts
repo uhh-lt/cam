29e92ae1f82b01469668e52e007c54340d216897
Ali Raza <raza4729@gmail.com>
2023-03-15 16:58:09 +0100

    apiURl commented

--- src/Frontend/camFrontend/src/environments/environment.prod.ts
+++ src/Frontend/camFrontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://ltdemos.informatik.uni-hamburg.de/cam-api',
-  suggestionsElasticsearchUrl: 'http://localhost:9200/suggestions-index/suggestions/_search'
+  production: true
+  // apiUrl: 'http://ltdemos.informatik.uni-hamburg.de/cam-api',
+  // suggestionsElasticsearchUrl: 'http://localhost:9200/suggestions-index/suggestions/_search'
 };
