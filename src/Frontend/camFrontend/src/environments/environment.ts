7e6f2ecbb07aab3b3f62a5a4a77ae9b28208948a
Ali Raza <raza4729@gmail.com>
2023-03-15 16:47:34 +0100

    Production mode switched to False

--- src/Frontend/camFrontend/src/environments/environment.ts
+++ src/Frontend/camFrontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000/cam',
-  suggestionsElasticsearchUrl: 'http://localhost:9200/suggestions-index/suggestions/_search'
+  production: false
+  // production: false,
+  // apiUrl: 'http://localhost:5000/cam',
+  // suggestionsElasticsearchUrl: 'http://localhost:9200/suggestions-index/suggestions/_search'
 };
