706e0c4696ab00ffd4cf2243f47d9daa52965965
Irina Nikishina <irina,nikishina@mail.ru>
2023-12-04 16:04:07 +0100

    update classifier

--- src/Backend/ml_approach/classify.py
+++ src/Backend/ml_approach/classify.py
@@ -9,18 +9,17 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "cam_pretrained"))
 
 from cam_pretrained.model_util import load_model
+from transformers import pipeline
 
 USE_HEURISTICS = True
 
 
-def classify_sentences(sentences, model):
-    if model == 'infersent':
-        model = load_model('data/infersent_model.pkl', glove_path='data/glove.840B.300d.txt',
-                           infersent_path='data/infersent.allnli.pickle')
-    else:
-        model = load_model('data/bow_model.pkl',
-                           glove_path=None, infersent_path=None)
-    df = DataFrame(model.predict_proba(sentences), columns=model.classes_)
+def classify_sentences(sentences, model="lilaspourpre/roberta-stance-compqa"):
+    pipe = pipeline("text-classification", model=model)
+    result = pipe(sentences, return_all_scores=True)
+    print(result)
+    print(df = DataFrame(result, columns=list(model.config.label2id.keys())))
+    df = DataFrame(result, columns=model.list(model.config.label2id.keys()))
     df['max'] = df.idxmax(axis=1)
     return df
 
